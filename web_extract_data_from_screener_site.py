0173f7f17c5876e61973bc7f80d38f5bffdd2e97
Anish Kumar <anish_kumar_ks@yahoo.com>
2025-09-20 20:01:26 +0530

    Add main app and scraper files

--- web_extract_data_from_screener_site.py
+++ web_extract_data_from_screener_site.py
@@ -117,7 +117,8 @@ def scrape_stock(stock_name="TCS"):
         results.append(f'[INFO]: URL: {url}')
 
         options = webdriver.ChromeOptions()
-        options.add_argument("--headless=new")
+        #options.add_argument("--headless=new")
+        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
